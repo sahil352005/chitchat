b07bf616165349940312d85c24b5f88a51a9874a
SAHIL PATIL <144355349+sahil352005@users.noreply.github.com>
2025-04-01 19:18:04 +0530

    New look

--- client/vite.config.js
+++ client/vite.config.js
@@ -5,3 +5,6 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
 })
+
+const API_URL = "https://chitchat1.vercel.app";
+fetch(`${API_URL}/api/messages`);
